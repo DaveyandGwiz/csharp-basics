fbf3753a2b2546a2ccfcdce7f372c7cf3ce577b8
David Galloway <Daveyandgwiz@gmail.com>
2024-02-16 00:37:08 -0600

    hello, world

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, I\'m a calculator!");
+      Console.WriteLine("Enter a number:");
+      int a = Convert.ToInt32(Console.ReadLine());      
+      Console.WriteLine("Enter another number:");
+      int b = Convert.ToInt32(Console.ReadLine());      
+      int sum = a + b;
+      Console.WriteLine(sum); // Print the sum of x + y    
+    }
+  }
+}
